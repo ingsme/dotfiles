c2c187888949e1f0aed28381e6926abbc403fd9d
ingsme <ingar.smedstad@uib.no>
2021-11-08 18:53:37 +0100

    config changes

--- base/nvim/lua/config/nvim-tree-rc.lua
+++ base/nvim/lua/config/nvim-tree-rc.lua
@@ -8,10 +8,8 @@ local utils = require("utils")
 local map = utils.map
 
 local g = vim.g
-g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
 g.nvim_tree_gitignore = 1
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_width_allow_resize = 1
@@ -27,8 +25,10 @@ g.nvim_tree_icons = {
 }
 
 require("nvim-tree").setup({
-	-- open nvim-tree at startup if no file is specified
-	-- open_on_setup = true
+  nvim_tree_ignore = { ".git", "node_modules", ".cache" },
+  nvim_tree_hide_dotfiles = 1,
+  -- open nvim-tree at startup if no file is specified
+  -- open_on_setup = true
 })
 
 map({ "n", "<C-n>", ":NvimTreeToggle<CR>" }) -- open/close
