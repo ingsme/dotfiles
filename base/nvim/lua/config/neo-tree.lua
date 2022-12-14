fe2e06650ed82f19052deadc6e500b45b517fc7e
Ingar Smedstad <ingar.smedstad@uib.no>
2022-12-14 16:39:02 +0100

    hmm

--- base/nvim/lua/config/neo-tree.lua
+++ base/nvim/lua/config/neo-tree.lua
@@ -1,4 +1,3 @@
-
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
@@ -206,4 +205,3 @@ require("neo-tree").setup({
 		},
 	},
 })
-
