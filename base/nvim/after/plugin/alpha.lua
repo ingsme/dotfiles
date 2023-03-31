d7c746006dc029c7f1d72df9d9f6687daffedfa5
Ingar Smedstad <ingar.smedstad@uib.no>
2023-03-31 12:20:24 +0200

    deleting the old

--- base/nvim/after/plugin/alpha.lua
+++
@@ -1,6 +0,0 @@
-local ok, alpha = pcall(require, 'alpha')
-if not ok then
-  return
-end
-
-alpha.setup(require'alpha.themes.dashboard'.config)
--- base/nvim/after/plugin/colorizer.lua
+++
@@ -1,6 +0,0 @@
-local ok, color = pcall(require, 'colorizer')
-if not ok then
-  return
-end
-
-color.setup({})
