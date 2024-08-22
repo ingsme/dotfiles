541918e426c622bb06e391f7f96eee3dd1cf602e
Ingar Smedstad <ingar.smedstad@uib.no>
2024-08-22 13:36:34 +0200

    Adding install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if which -s brew; then
+  echo "Homebrew already installed"
+else
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  case $(uname -s) in
+    Linux)
+      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+      ;;
+    Darwin)
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+      ;;
+    *)
+      echo "This should not happen. Something is wrong!"
+      ;;
+  esac
+fi
+
+brew install chezmoi
+chezmoi init --ssh --apply ingsme
