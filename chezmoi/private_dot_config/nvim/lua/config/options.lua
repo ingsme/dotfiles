22deb7ea18cd2087e70e6f10db08deb4c4e86c22
Ingar Smedstad <ingar.smedstad@uib.no>
2025-03-26 09:20:57 +0100

    ok

--- chezmoi/private_dot_config/nvim/lua/config/options.lua
+++ chezmoi/private_dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.python3_host_prog = '/scratch/pyenv/versions/3.12.2/envs/neovim3/bin/python3'
