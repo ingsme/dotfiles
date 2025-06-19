1ba195bdbd789b9f64538abcba029130a28d702a
Ingar Smedstad <ingar.smedstad@uib.no>
2025-06-19 10:31:18 +0200

    update

--- chezmoi/private_dot_config/nvim/lua/config/options.lua
+++ chezmoi/private_dot_config/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.python3_host_prog = '/scratch/pyenv/versions/3.12.2/envs/neovim3/bin/python3'
+vim.g.lazyvim_blink_main = true
