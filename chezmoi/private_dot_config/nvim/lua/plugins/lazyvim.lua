4e6427594d3e730050369458980587a2e696765c
Ingar Smedstad <ingar.smedstad@uib.no>
2025-11-10 10:42:12 +0100

    ok

--- chezmoi/private_dot_config/nvim/lua/plugins/lazyvim.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/lazyvim.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    'catppuccin/nvim',
-    opts = function(_, opts)
-      local module = require('catppuccin.groups.integrations.bufferline')
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
-  },
+  -- {
+  --   'catppuccin/nvim',
+  --   opts = function(_, opts)
+  --     local module = require('catppuccin.groups.integrations.bufferline')
+  --     if module then
+  --       module.get = module.get_theme
+  --     end
+  --     return opts
+  --   end,
+  -- },
   {
     'folke/snacks.nvim',
     opts = {
